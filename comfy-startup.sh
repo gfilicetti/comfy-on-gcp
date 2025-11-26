3c56a4e456fbe866ad7f87e753e573d20008b3ec
Gino Filicetti <ginof@google.com>
2025-11-26 18:32:14 -0500

    Fixed startup disk to remove redundancies

--- comfy-startup.sh
+++ comfy-startup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # --- Configuration ---
-USER_NAME="admin_ginof_altostrat_com"
-USER_HOME="/home/$USER_NAME"
+USER_NAME=$USER
+USER_HOME=$HOME
 # --- End Configuration ---
 
 # 1. Mount the Persistent Disk (Required Step)
@@ -43,27 +43,6 @@ if [ ! -d "$USER_HOME/ComfyUI" ]; then
         rm -rf \"$USER_HOME/ComfyUI/models\"
         ln -s /mnt/disks/models \"$USER_HOME/ComfyUI/models\"
     "
-
-    # a. Clone ComfyUI as the user
-    sudo -u $USER_NAME git clone https://github.com/comfyanonymous/ComfyUI.git "$USER_HOME/ComfyUI"
-
-    # b. Create and activate venv (CRITICAL FIX)
-    sudo -u $USER_NAME python3 -m venv "$USER_HOME/ComfyUI/venv"
-
-    # c. Install dependencies inside the venv
-    # We use the full path to the venv python to avoid activation issues in startup script
-    sudo -u $USER_NAME "$USER_HOME/ComfyUI/venv/bin/pip" install -r "$USER_HOME/ComfyUI/requirements.txt"
-
-    # d. Install ComfyUI Manager
-    sudo -u $USER_NAME git clone https://github.com/ltdrdata/ComfyUI-Manager.git "$USER_HOME/ComfyUI/custom_nodes/ComfyUI-Manager"
-
-    # e. Re-establish the models folder symlink (from previous steps)
-    # This assumes the base setup script in the past created the symlink structure.
-    # We assume the models folder is empty/removed after the initial setup.
-    sudo -u $USER_NAME rm -rf "$USER_HOME/ComfyUI/models"
-    sudo -u $USER_NAME ln -s /mnt/disks/models "$USER_HOME/ComfyUI/models"
-    
-    echo "ComfyUI installation complete."
 fi
 
 # 3. Start ComfyUI as the non-root user (using venv Python)
